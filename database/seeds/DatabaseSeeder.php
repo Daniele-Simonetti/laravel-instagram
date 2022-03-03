2feca75363b64fc6076ac7d413dea72c4aa513ee
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-03-03 12:35:01 +0100

    db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(PhotoSeeder::class);
+        $this->call(UserInfoSeeder::class);
     }
 }
