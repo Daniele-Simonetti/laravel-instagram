04b2fec301c706b5418443804f5e96f36ec4733b
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-03-03 13:03:12 +0100

    add home

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
-        $this->call(CommentSeeder::class);
         $this->call(PhotoSeeder::class);
+        $this->call(CommentSeeder::class);
         $this->call(UserInfoSeeder::class);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach ($photos as $photo)
+                    <div class="card mb-3">
+                        <img src="{{ $photo->url }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title">{{ $photo->user()->first()->name }}</h3>
+                            <h5 class="card-title">{{ $photo->name }}</h5>
+                            <p class="card-text">{{ $photo->description }}</p>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($photo->comments()->get() as $comment)
+                                <li class="list-group-item"><strong>{{ $comment->user()->first()->name }}:</strong> {{ $comment->content }}</li>
+                            @endforeach
+                        </ul>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col">
+            {{ $photos->links() }}
+        </div>
+    </div>
+    </div>
 @endsection
